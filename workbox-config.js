ac405ea856b9e9c992f49a6879793402781d0e63
danylokarpenko <karpenkodanylo@gmail.com>
2020-05-04 18:19:30 +0300

    Fix workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -3,24 +3,10 @@ module.exports = {
   globPatterns: [
     '**/*.{png,svg,ico,jpg,mp3,css,html}'
   ],
+  globIgnores: [
+    '**/node_modules/**/*',
+    'sw.js',
+    'workbox-*.js'
+  ],
   swDest: 'build/sw.js',
-
-  // Define runtime caching rules.
-  runtimeCaching: [{
-    // Match any request that ends with .png, .jpg, .jpeg or .svg.
-    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-    // Apply a cache-first strategy.
-    handler: 'CacheFirst',
-
-    options: {
-      // Use a custom cache name.
-      cacheName: 'images',
-
-      // Only cache 10 images.
-      expiration: {
-        maxEntries: 10,
-      },
-    },
-  }],
 };

b0c1cad759f2f8f442575569110e32972e438ab4
danylokarpenko <karpenkodanylo@gmail.com>
2020-05-04 18:13:54 +0300

    Add register of sw.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Homepage
+[![Build Status](https://travis-ci.com/danylokarpenko/homepage.svg?branch=develop)](https://travis-ci.com/danylokarpenko/homepage)
+
 My very own personal website. Basically this is just a résumé.
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,26 @@
 module.exports = {
-  globDirectory: './',
+  globDirectory: 'build/',
   globPatterns: [
     '**/*.{png,svg,ico,jpg,mp3,css,html}'
   ],
-  swDest: 'sw.js'
+  swDest: 'build/sw.js',
+
+  // Define runtime caching rules.
+  runtimeCaching: [{
+    // Match any request that ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'images',
+
+      // Only cache 10 images.
+      expiration: {
+        maxEntries: 10,
+      },
+    },
+  }],
 };
